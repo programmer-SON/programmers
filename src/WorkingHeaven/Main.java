6441a6ccfd561dedd8e5d1191767b479f7cad20a
sjw3957 <sjw3957@gmail.com>
2021-08-25 23:02:53 +0900

    보행자 천국 다른풀이

--- src/WorkingHeaven/Main.java
+++ src/WorkingHeaven/Main.java
@@ -2,25 +2,40 @@ class Solution {
     int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
         int answer = 0;
-        int dp1[][] = new int[m+1][n+1];
-        int dp2[][] = new int[m+1][n+1];
+        int dp[][][] = new int[m+1][n+1][2];
         
-        dp1[1][1] = dp2[1][1] = 1;
         
-        for(int i=1; i<=m;i++){
-            for(int j=1; j<=n;j++){
-                if(cityMap[i-1][j-1] == 0){
-                    dp2[i][j] = dp1[i][j] += (dp1[i][j-1] + dp2[i-1][j]) % MOD;
-                    
-                }else if(cityMap[i-1][j-1] == 1){
-                   dp1[i][j] = dp2[i][j] = 0; 
+        for(int i=1; i<m;i++){
+            if(cityMap[i][0] == 1) break;
+            dp[i][0][1] = 1;
+        }
+        
+        for(int i=1; i<n;i++){
+            if(cityMap[0][i] == 1) break;
+            dp[0][i][0] = 1;
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1; j<n;j++){
+                if(cityMap[i-1][j] == 0){
+                    dp[i][j][1] += (dp[i-1][j][0] + dp[i-1][j][1]) % MOD;
+                }else if(cityMap[i-1][j] == 1){
+                    dp[i][j][1] = 0;
                 }else{
-                    dp1[i][j] = dp1[i][j-1];
-                    dp2[i][j] = dp2[i-1][j];
+                    dp[i][j][1] = dp[i-1][j][1];
                 }
+                
+                if(cityMap[i][j-1] == 0){
+                    dp[i][j][0] += (dp[i][j-1][0] + dp[i][j-1][1]) % MOD;
+                }else if(cityMap[i][j-1] == 1){
+                    dp[i][j][0] = 0;
+                }else{
+                    dp[i][j][0] = dp[i][j-1][0];
+                }                
             }
         }
         
-        return dp1[m][n] % MOD;
+        
+        return (dp[m-1][n-1][0] + dp[m-1][n-1][1]) % MOD;
     }
 }
